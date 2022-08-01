6d5a4651c4580c05fc0cf9f40ff61227f0af7f35
osmon ahmagjekaj <osmonahmagjekaj@odysseyprime.nl>
2022-08-01 09:55:20 +0200

    Class that hold category value data.

---
+++ app/io/exercise/api/models/CategoryValueData.java
@@ -0,0 +1,15 @@
+package io.exercise.api.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode
+public class CategoryValueData {
+    private String category;
+    private int value;
+}
