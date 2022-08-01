e0e5e5e6bc95d25c8febc3b57c2268a406ce570a
osmon ahmagjekaj <osmonahmagjekaj@odysseyprime.nl>
2022-08-01 09:56:17 +0200

    Request exception class.

---
+++ app/io/exercise/api/exceptions/RequestException.java
@@ -0,0 +1,29 @@
+package io.exercise.api.exceptions;
+
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+public @Data class RequestException extends Exception {
+	private static final long serialVersionUID = 1L;
+	// either String or List<String>
+    private Object description;
+    private int statusCode;
+
+    public RequestException(int statusCode, Object message) {
+        super(message.toString());
+        this.description = message;
+        this.statusCode = statusCode;
+    }
+    
+    @Override
+    public String getMessage() {
+        return description.toString();
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
+
+
