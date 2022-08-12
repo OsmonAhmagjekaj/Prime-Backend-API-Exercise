f5d3f7cbbe93703d0ef00431451c59b9fb634411
osmon ahmagjekaj <osmonahmagjekaj@odysseyprime.nl>
2022-08-12 10:25:42 +0200

    Now User can return its access ids and check if it has access in an BaseModel object.

--- app/io/exercise/api/models/User.java
+++ app/io/exercise/api/models/User.java
@@ -1,14 +1,19 @@
 package io.exercise.api.models;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.exercise.api.utils.ServiceUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.bson.codecs.pojo.annotations.BsonIgnore;
 
 import javax.validation.constraints.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -24,5 +29,22 @@ public class User extends BaseModel {
     @Size(min = 5, message = "should have at least 5 characters")
     String password;
 
-    List<Role> roles = new ArrayList<>();
+    List<String> roles = new ArrayList<>();
+
+    @JsonIgnore
+    @BsonIgnore
+    public boolean hasReadWriteAccessFor (BaseModel object) {
+        return (object.getWriteACL().size() == 0 && object.getReadACL().size() == 0)
+                || object.getWriteACL().contains(this.getId().toString())
+                || object.getWriteACL().contains("*")
+                || ServiceUtils.containElementsInCommon(object.getWriteACL(), this.getRoles());
+    }
+
+    @JsonIgnore
+    @BsonIgnore
+    public List<String> getAccessIds () {
+        return Stream.of(List.of(this.getId().toString()), this.getRoles(), List.of("*"))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
 }
