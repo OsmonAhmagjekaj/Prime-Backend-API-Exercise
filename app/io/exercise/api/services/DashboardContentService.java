7ebc0725283c622e3fde3f58e8569c6882141d32
osmon ahmagjekaj <osmonahmagjekaj@odysseyprime.nl>
2022-08-12 10:28:45 +0200

    Added pagination on the all action method and made some slight changes.

--- app/io/exercise/api/services/DashboardContentService.java
+++ app/io/exercise/api/services/DashboardContentService.java
@@ -1,12 +1,10 @@
 package io.exercise.api.services;
 
 import com.google.inject.Inject;
+import com.mongodb.MongoException;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
-import com.mongodb.client.result.DeleteResult;
-import com.mongodb.client.result.InsertOneResult;
-import com.mongodb.client.result.UpdateResult;
 import io.exercise.api.exceptions.RequestException;
 import io.exercise.api.models.User;
 import io.exercise.api.models.dashboard.Content;
@@ -31,28 +29,24 @@ public class DashboardContentService {
     @Inject
     IMongoDB mongoDB;
 
-    public CompletableFuture<List<Content>> all (User user, String id) {
+    public CompletableFuture<List<Content>> all (int skip, int limit, User user, String id) {
         return CompletableFuture.supplyAsync(() -> {
             try {
                 return mongoDB.getMongoDatabase()
                         .getCollection("dashboardsContent", Content.class)
                         .find(Filters.and(
                                 Filters.eq("dashboardId", new ObjectId(id)),
-                                Filters.or(
-                                        Filters.in("readACL", user.getId().toString()),
-                                        Filters.in("readACL", ServiceUtils.getRolesFrom(user)),
-                                        Filters.in("writeACL", user.getId().toString()),
-                                        Filters.in("writeACL", ServiceUtils.getRolesFrom(user)),
-                                        Filters.and(
-                                                Filters.eq("readACL", new ArrayList<String>()),
-                                                Filters.eq("writeACL", new ArrayList<String>())
-                                        )
-                                )
+                                ServiceUtils.getReadAccessFilterFor(user.getAccessIds())
                         ))
+                        .skip(skip)
+                        .limit(limit)
                         .into(new ArrayList<>());
+            } catch (MongoException ex) {
+                ex.printStackTrace();
+                throw new CompletionException(new RequestException(Http.Status.INTERNAL_SERVER_ERROR, "Mongo error " + ex));
             } catch (Exception ex) {
                 ex.printStackTrace();
-                throw new CompletionException(new RequestException(Http.Status.BAD_REQUEST, Json.toJson("Could not fetch data!")));
+                throw new CompletionException(new RequestException(Http.Status.INTERNAL_SERVER_ERROR, ex));
             }
         }, ec.current());
     }
@@ -60,23 +54,29 @@ public class DashboardContentService {
     public CompletableFuture<Content> save(User user, Content content, String id) {
         return CompletableFuture.supplyAsync(() -> {
             try {
+                Dashboard dashboard = mongoDB.getMongoDatabase()
+                        .getCollection("dashboards", Dashboard.class)
+                        .find(Filters.eq("id", new ObjectId(id)))
+                        .first();
+                if (dashboard == null) {
+                    throw new CompletionException(new RequestException(Http.Status.BAD_REQUEST, "No dashboard exists with such id!"));
+                }
+
                 MongoCollection<Content> collection = mongoDB.getMongoDatabase()
                         .getCollection("dashboardsContent", Content.class);
 
                 content.setDashboardId(new ObjectId(id));
                 content.getReadACL().add(user.getId().toString());
                 content.getWriteACL().add(user.getId().toString());
+                collection.insertOne(content);
 
-                InsertOneResult result = collection.insertOne(content);
-                if (!result.wasAcknowledged() || result.getInsertedId() == null) {
-                    throw new CompletionException(new RequestException(Http.Status.BAD_REQUEST, Json.toJson("Could not insert data!")));
-                }
-
-                //return collection.find(Filters.eq("_id", result.getInsertedId())).first();
                 return content;
+            } catch (MongoException ex) {
+                ex.printStackTrace();
+                throw new CompletionException(new RequestException(Http.Status.INTERNAL_SERVER_ERROR, "Mongo error " + ex));
             } catch (Exception ex) {
                 ex.printStackTrace();
-                throw ex;
+                throw new CompletionException(new RequestException(Http.Status.INTERNAL_SERVER_ERROR, ex));
             }
         }, ec.current());
     }
@@ -84,6 +84,14 @@ public class DashboardContentService {
     public CompletableFuture<Content> update(User user, Content content, String id) {
         return CompletableFuture.supplyAsync(() -> {
             try {
+                Dashboard dashboard = mongoDB.getMongoDatabase()
+                        .getCollection("dashboards", Dashboard.class)
+                        .find(Filters.eq("id", new ObjectId(id)))
+                        .first();
+                if (dashboard == null) {
+                    throw new CompletionException(new RequestException(Http.Status.BAD_REQUEST, "No dashboard exists with such id!"));
+                }
+
                 MongoCollection<Content> collection = mongoDB.getMongoDatabase()
                         .getCollection("dashboardsContent", Content.class);
                 content.setDashboardId(new ObjectId(id));
@@ -94,24 +102,23 @@ public class DashboardContentService {
                     throw new CompletionException(new RequestException(Http.Status.NOT_FOUND, Json.toJson("Could not find data!")));
                 }
 
-                if (!ServiceUtils.hasReadWriteAccess(user, foundContent)) {
+                if (!user.hasReadWriteAccessFor(foundContent)) {
                     throw new CompletionException(new RequestException(Http.Status.FORBIDDEN, Json.toJson("FORBIDDEN!")));
                 }
                 content.getReadACL().addAll(foundContent.getReadACL());
                 content.getWriteACL().addAll(foundContent.getWriteACL());
-
                 collection.replaceOne(Filters.eq("_id", content.getId()), content);
-//                if (!result.wasAcknowledged() || result.getMatchedCount() == 0) {
-//                    throw new CompletionException(new RequestException(Http.Status.BAD_REQUEST, Json.toJson("Could not update data!")));
-//                }
 
                 return content;
             } catch (CompletionException ex) {
                 ex.printStackTrace();
-                throw new CompletionException(ex);
+                throw ex;
+            } catch (MongoException ex) {
+                ex.printStackTrace();
+                throw new CompletionException(new RequestException(Http.Status.INTERNAL_SERVER_ERROR, "Mongo error " + ex));
             } catch (Exception ex) {
                 ex.printStackTrace();
-                throw ex;
+                throw new CompletionException(new RequestException(Http.Status.INTERNAL_SERVER_ERROR, ex));
             }
         }, ec.current());
     }
@@ -128,22 +135,21 @@ public class DashboardContentService {
                     throw new CompletionException(new RequestException(Http.Status.NOT_FOUND, Json.toJson("Could not find data!")));
                 }
 
-                if (!ServiceUtils.hasReadWriteAccess(user, foundContent)) {
+                if (!user.hasReadWriteAccessFor(foundContent)) {
                     throw new CompletionException(new RequestException(Http.Status.FORBIDDEN, Json.toJson("FORBIDDEN!")));
                 }
-
                 collection.deleteOne(Filters.eq("_id", content.getId()));
-//                if (!result.wasAcknowledged() || result.getDeletedCount() == 0) {
-//                    throw new CompletionException(new RequestException(Http.Status.BAD_REQUEST, Json.toJson("Could not delete data!")));
-//                }
 
                 return content;
             } catch (CompletionException ex) {
                 ex.printStackTrace();
                 throw ex;
+            } catch (MongoException ex) {
+                ex.printStackTrace();
+                throw new CompletionException(new RequestException(Http.Status.INTERNAL_SERVER_ERROR, "Mongo error " + ex));
             } catch (Exception ex) {
                 ex.printStackTrace();
-                throw new CompletionException(new RequestException(Http.Status.BAD_REQUEST, Json.toJson("Internal error!")));
+                throw new CompletionException(new RequestException(Http.Status.INTERNAL_SERVER_ERROR, ex));
             }
         }, ec.current());
     }
